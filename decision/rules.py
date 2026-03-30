c9387ac076c9018a98234cad593858ea449f968c
apokalypsengamer-design <apokalypsengamer@gmail.com>
2026-03-30 22:57:07 +0200

    Refactor RuleEngine to improve steering logic

--- decision/rules.py
+++ decision/rules.py
@@ -2,50 +2,45 @@ from config import Config
 from core.state import GameState
 from input.actions import Action
 
+
 class RuleEngine:
     def __init__(self, config: Config):
-        self.config = config
+        self._dz = config.gameplay.steer_dead_zone
 
     def evaluate(self, state: GameState, frame: int) -> Action:
         phase = state.phase
-
         if phase == "attack":
             return self._attack(state)
-        elif phase == "defense":
+        if phase == "defense":
             return self._defense(state)
-        elif phase == "boost_collect":
-            return self._boost_collect(frame)
-        else:
-            return self._rotate(state)
+        if phase == "boost_collect":
+            return self._boost_collect(state)
+        return self._rotate(state)
 
     def _attack(self, state: GameState) -> Action:
-        a = Action(forward=True, boost=True)
-        return self._steer_to_ball(a, state)
+        return self._steer_to_ball(Action(forward=True, boost=True), state)
 
     def _defense(self, state: GameState) -> Action:
         a = Action(forward=True, boost=True)
-        a.steer_left  = state.ball_x < 0.45
-        a.steer_right = state.ball_x > 0.55
+        a.steer_left  = state.ball_x < (0.5 - self._dz)
+        a.steer_right = state.ball_x > (0.5 + self._dz)
         return a
 
     def _rotate(self, state: GameState) -> Action:
-        a = Action(forward=True)
-        return self._steer_to_ball(a, state)
+        return self._steer_to_ball(Action(forward=True), state)
 
-    def _boost_collect(self, frame: int) -> Action:
+    def _boost_collect(self, state: GameState) -> Action:
         a = Action(forward=True)
-        if (frame // 20) % 2 == 0:
-            a.steer_right = True
+        if state.ball_visible:
+            a.steer_left  = state.ball_x < (0.5 - self._dz)
+            a.steer_right = state.ball_x > (0.5 + self._dz)
         else:
-            a.steer_left = True
+            a.steer_right = True
         return a
 
-    @staticmethod
-    def _steer_to_ball(action: Action, state: GameState) -> Action:
+    def _steer_to_ball(self, action: Action, state: GameState) -> Action:
         if not state.ball_visible:
             return action
-        if state.ball_x < 0.45:
-            action.steer_left, action.steer_right = True, False
-        elif state.ball_x > 0.55:
-            action.steer_right, action.steer_left = True, False
+        action.steer_left  = state.ball_x < (0.5 - self._dz)
+        action.steer_right = state.ball_x > (0.5 + self._dz)
         return action
